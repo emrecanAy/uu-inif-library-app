21577f9e8b6ce0a2f162b070b907b50e6abf7a33
Şaban Dönmez <eren.donmez.11@gmail.com>
2022-03-24 14:51:36 +0300

    Test

--- uu-library-app/Program.cs
+++ uu-library-app/Program.cs
@@ -17,6 +17,7 @@ namespace uu_library_app
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+            Console.WriteLine("Test");
         }
     }
 }
